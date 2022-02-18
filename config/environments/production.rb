ed193d759858766f17f98a5d8a0c80ae9d6c67e4
chaytee <shimashima.works@gmail.com>
2022-02-19 08:40:47 +0900

    websoket 修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://futasu-v1-api.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://futasu-v1-front.herokuapp.com/', /http:\/\/example.*/ ]
+  #config.action_cable.url = 'wss://futasu-v1-api.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://futasu-v1-api.herokuapp.com/', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
